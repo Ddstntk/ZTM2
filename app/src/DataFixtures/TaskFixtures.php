d7058d4251450cf06ee94f4523b74bf8033fed6f
Konrad Szewczuk <konrabana@gmail.com>
2020-08-22 03:49:30 +0200

    Doctrine relations done

--- app/src/DataFixtures/TaskFixtures.php
+++ app/src/DataFixtures/TaskFixtures.php
@@ -6,12 +6,13 @@
 namespace App\DataFixtures;
 
 use App\Entity\Task;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 
 /**
  * Class TaskFixtures.
  */
-class TaskFixtures extends AbstractBaseFixtures
+class TaskFixtures extends AbstractBaseFixtures implements DependentFixtureInterface
 {
     /**
      * Load data.
@@ -20,14 +21,27 @@ class TaskFixtures extends AbstractBaseFixtures
      */
     public function loadData(ObjectManager $manager): void
     {
-        for ($i = 0; $i < 10; ++$i) {
+        $this->createMany(50, 'tasks', function ($i) {
             $task = new Task();
             $task->setTitle($this->faker->sentence);
             $task->setCreatedAt($this->faker->dateTimeBetween('-100 days', '-1 days'));
             $task->setUpdatedAt($this->faker->dateTimeBetween('-100 days', '-1 days'));
-            $this->manager->persist($task);
-        }
+            $task->setCategory($this->getRandomReference('categories'));
+
+            return $task;
+        });
 
         $manager->flush();
+    }
+
+    /**
+     * This method must return an array of fixtures classes
+     * on which the implementing class depends on.
+     *
+     * @return array Array of dependencies
+     */
+    public function getDependencies(): array
+    {
+        return [CategoryFixtures::class];
     }
 }
